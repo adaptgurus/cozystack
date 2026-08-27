8a7d3bfa523a1f941bc89a9b87ed2d61594b0329
adaptgurus <73125939+adaptgurus@users.noreply.github.com>
2026-08-27 16:42:03 +0000

    fix(hci): use Talos API backend for network transactions

--- cmd/network-fabric-controller/main.go
+++ cmd/network-fabric-controller/main.go
@@ -22,12 +22,11 @@ import (
 )
 
 func main() {
-	var metricsAddr, healthAddr, talosconfig, talosctl string
+	var metricsAddr, healthAddr, talosconfig string
 	var requeue, tryTimeout time.Duration
 	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "metrics bind address")
 	flag.StringVar(&healthAddr, "health-probe-bind-address", ":8081", "health probe bind address")
 	flag.StringVar(&talosconfig, "talosconfig", "/var/run/secrets/talos.dev/config", "path to Talos client configuration")
-	flag.StringVar(&talosctl, "talosctl", "/usr/local/bin/talosctl", "path to talosctl")
 	flag.DurationVar(&requeue, "health-requeue", 2*time.Minute, "periodic NetworkFabric health recheck")
 	flag.DurationVar(&tryTimeout, "talos-try-timeout", 2*time.Minute, "Talos try-mode automatic rollback timeout")
 	flag.Parse()
@@ -54,7 +53,7 @@ func main() {
 			if err != nil {
 				return nil, err
 			}
-			return &networkfabric.TalosctlAdapter{Binary: talosctl, Talosconfig: talosconfig, Endpoint: endpoint, TryTimeout: tryTimeout}, nil
+			return &networkfabric.TalosAPIAdapter{Talosconfig: talosconfig, Endpoint: endpoint, TryTimeout: tryTimeout}, nil
 		},
 	}
 	reconciler := networkfabriccontroller.NewInstrumentedReconciler(baseReconciler)
