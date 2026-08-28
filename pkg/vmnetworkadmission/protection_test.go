aff2567366ce4ba29dfee9c589acf63a135c3937
adaptgurus <73125939+adaptgurus@users.noreply.github.com>
2026-08-28 08:12:18 +0000

    test(virtualization): fix protection admission test imports

--- pkg/vmnetworkadmission/protection_test.go
+++ pkg/vmnetworkadmission/protection_test.go
@@ -13,6 +13,7 @@ import (
 	"time"
 
 	admissionv1 "k8s.io/api/admission/v1"
+	authenticationv1 "k8s.io/api/authentication/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
